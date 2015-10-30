12fc3a4108d7efe172c5a6b2987ab02e5790176f
David Zhang <david8zhang@berkeley.edu>
2015-10-29 22:04:22 -0700

    finished project

---
+++ proj1/app/controllers/pokemons_controller.rb
@@ -0,0 +1,33 @@
+class PokemonsController < ApplicationController
+  def capture
+    pokemon = Pokemon.find(params[:id])
+    pokemon.trainer_id = current_trainer.id
+    pokemon.save
+    redirect_to root_path
+  end
+
+  def damage
+    pokemon = Pokemon.find(params[:id])
+    pokemon.health = pokemon.health - 10
+    pokemon.save
+    if pokemon.health <= 0
+      pokemon.destroy
+    end
+    redirect_to :back
+  end
+
+  def new
+    @pokemon = Pokemon.new
+  end
+
+  def create
+    name = params[:pokemon][:name]
+    @pokemon = Pokemon.create(name: name, level: 1, health: 100, trainer_id: current_trainer.id)
+    if !@pokemon.valid?
+      flash[:error] = @pokemon.errors.full_messages.to_sentence
+      redirect_to :back
+    else
+      redirect_to "/trainers/" + current_trainer.id.to_s
+    end
+  end
+end
